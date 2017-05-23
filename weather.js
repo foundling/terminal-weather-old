6bcb07eede43521f6ea6a4520ca10d4a2d1ed9b9
Alex Ramsdell <alexramsdell@gmail.com>
2017-05-23 14:59:14 -0700

    wip

--- weather.js
+++ weather.js
@@ -1,91 +1,98 @@
 #!/usr/bin/env node
 
-/*
- *
- * write to file every x hours and read from it for x hours. 
- * To Do: make it location based using ip-api.com to get the current city
- *
- */
+const http = require('http');
+const querystring = require('querystring');
+const WEATHER_API_KEY = require('./config').API_KEY; 
+const weatherHost = 'api.openweathermap.org';
+const weatherPath = '/data/2.5/weather';
+const geolocationHost = 'ip-api.com';
+const geolocationPath = '/json';
 
-process.env.NODE_PATH="/Users/alexr/.node/lib/node_modules";
+function getLocation() {
 
-var fs = require('fs');
-var moment = require('/Users/alexr/.node/lib/node_modules/moment');
-var http = require('http');
-var city = process.argv[2] || null;
-var KtoF = function(K) {
-  // kelvin to farenheit equation
-  return parseInt( ((parseFloat(K)*9)/5) - 459.67);
-};
-var cachePeriod = 100;
+    return new Promise((resolve, reject) => {
 
+        let result = '';
 
-
-var checkTime = function() {
-  fs.readFile('/users/alexr/.weather', function(err, data){
-      if (err) return console.log(err);
-
-      var start = parseTimeData(data);
-      var now = getTimeAsInt();
-      timeElapsed = now - start;
-
-      if (!start) readWeatherFromApi();
-      if (timeElapsed > cachePeriod) readWeatherFromApi(); 
-      else readWeatherFromDisk();
-  });
-
-};
-
-var getTimeAsInt = function() {
-  return parseInt(moment().format('YYYYMMDDhhmmss'));
-};
-
-var parseTimeData = function(data) {
-  return parseInt(data.toString().split('\n')[1].trim());
-};
-
-var readWeatherFromDisk = function() {
-  fs.readFile('/users/alexr/.weather', function(err, data){
-      if (err) return console.log(err);
-      console.log('d:',data.toString().split('\n')[0].trim());
-  });
-};
-
-var readWeatherFromApi = function() {
-  http.request({host: 'api.openweathermap.org',
-      path: '/data/2.5/weather?q=' + city + ',us'
-    }, 
-    function(response) {
-      var buffer = '';
-
-      response.on('data', function (chunk) {
-        buffer += chunk;
-      });
-
-      response.on('end', function () {
-        var time;
-        var weatherData = JSON.parse(buffer);
-        var rv = {
-          temp: KtoF(weatherData['main']['temp']),
-          low: KtoF(weatherData['main']['temp_min']),
-          high: KtoF(weatherData['main']['temp_max']),
-          desc: weatherData['weather'][0]['description'],
+        const options = {
+            host: geolocationHost,
+            path: geolocationPath
         };
 
-        var terminalWidgetString = 
-            rv['temp'] +
-            ' °F, ' +
-            rv['desc'];
+        http.request(options, (response) => {
 
-        time = getTimeAsInt();
-        
+            response.on('data', function(data) {
+                result += data;
+            });
 
-        fs.writeFile('/Users/alexr/.weather', [terminalWidgetString,time].join('\n'), function(err){
-           if (err) return console.log(err);
-        });
-        console.log('a:',terminalWidgetString);
+            response.on('end', function() {
+                resolve(result);
+            });
+
+            response.on('error', function(data) {
+                reject(data);
+            });
+
+        }).end();
+
     });
-  }).end();
-};
 
-checkTime();
+}
+
+function getWeather({ city, countryCode}) {
+
+    console.log(city);
+    return new Promise((resolve, reject) => {
+
+        let result = '';
+
+        let qs = querystring.stringify({ 
+
+            city: city, 
+            countryCode: countryCode, 
+            APPID: WEATHER_API_KEY
+
+        }); 
+
+        const options = {
+            host: weatherHost,
+            path: `${weatherPath}?q=${qs}`
+        };
+
+        http.request(options, (response) => {
+
+            response.on('data', function(data) {
+                result += 'data';
+            });
+
+            response.on('end', function(data) {
+                resolve(data);
+            });
+
+            response.on('error', function(data) {
+                reject(data);
+            });
+
+        }).end();
+    });
+}
+
+getLocation().then(data => {
+
+    let { city, countryCode } = data;
+
+    return getWeather({
+
+        city,
+        countryCode
+
+    });
+
+})
+.then(data => {
+
+    console.log(data);
+
+}).catch(e => {
+    throw e;
+});
