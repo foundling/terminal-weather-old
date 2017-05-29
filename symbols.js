bfd54fd7f0717cae8634ee3998eea7e37fad63d8
Alex Ramsdell <alexramsdell@gmail.com>
2017-05-28 18:28:44 -0700

    increase cache interval. add symbol map. add binary. [todo: user-defined display configuration involving the config.json. install logic ]

---
+++ symbols.js
@@ -0,0 +1,17 @@
+module.exports = exports = weatherToColorMap = {
+
+    icons: {
+        'clear': '🌞',
+        'clouds': '⛅',
+        'rain': '☂',
+    },
+    coloredText: {
+        'clear': 'yellow', 
+        'rain': 'blue',
+        'clouds': 'gray',
+        'snow': 'white',
+    }
+
+};
+
+
