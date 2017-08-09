467ab53d9ac2e94c6d935025bd6a58d513aab1a1
Alex Ramsdell <alexramsdell@gmail.com>
2017-08-09 15:59:46 -0700

    update display

--- cli/list.js
+++ cli/list.js
@@ -1,4 +1,14 @@
-const display = require('../data/display');
+const { text, icon } = require('../data/display');
+
 module.exports = function() {
-    console.log(display);
+
+    Object.keys(text).forEach(key => {
+        console.log(`${ key }\t| ${text[key]}`);
+    });
+
+    Object.keys(icon).forEach(key => {
+        console.log(`${ key } (am)\t| ${ icon[key].day }`);
+        console.log(`${ key } (pm)\t| ${ icon[key].night }`);
+    });
+
 };
--- data/display.js
+++ data/display.js
@@ -1,41 +1,52 @@
-Reset = "\x1b[0m";
-Bright = "\x1b[1m";
-Dim = "\x1b[2m";
-Underscore = "\x1b[4m";
-Blink = "\x1b[5m";
-Reverse = "\x1b[7m";
-Hidden = "\x1b[8m";
+const ansiColors = {
 
-FgBlack = "\x1b[30m";
-FgRed = "\x1b[31m";
-FgGreen = "\x1b[32m";
-FgYellow = "\x1b[33m";
-FgBlue = "\x1b[34m";
-FgMagenta = "\x1b[35m";
-FgCyan = "\x1b[36m";
-FgWhite = "\x1b[37m";
+    reset:  "\x1b[0m",
+    bright: "\x1b[1m",
+    dim: "\x1b[2m",
+    underscore: "\x1b[4m",
+    blink: "\x1b[5m",
+    reverse: "\x1b[7m",
+    hidden: "\x1b[8m",
 
-BgBlack = "\x1b[40m";
-BgRed = "\x1b[41m";
-BgGreen = "\x1b[42m";
-BgYellow = "\x1b[43m";
-BgBlue = "\x1b[44m";
-BgMagenta = "\x1b[45m";
-BgCyan = "\x1b[46m";
-BgWhite = "\x1b[47m";
+    fgBlack: "\x1b[30m",
+    fgLightGray: "\x1b[90m",
+    fgLightRed: "\x1b[91m",
+    fgRed: "\x1b[31m",
+    fgGreen: "\x1b[32m",
+    fgYellow:  "\x1b[33m",
+    fgBlue: "\x1b[34m",
+    fgLightBlue: "\x1b[94m",
+    fgMagenta: "\x1b[35m",
+    fgCyan: "\x1b[36m",
+    fgWhite: "\x1b[37m",
+
+    bgBlack: "\x1b[40m",
+    bgLightGray: "\x1b[100m",
+    bgLightRed: "\x1b[101m",
+    bgLightBlue: "\x1b[104m",
+    bgRed: "\x1b[41m",
+    bgGreen: "\x1b[42m",
+    bgYellow: "\x1b[43m",
+    bgBlue: "\x1b[44m",
+    bgMagenta: "\x1b[45m",
+    bgCyan: "\x1b[46m",
+    bgWhite: "\x1b[47m",
+    bgLightWhite: "\x1b[107m",
+
+};
 
 module.exports = exports = weatherToColorMap = {
     text: {
-        clear: `${FgYellow} clear ${Reset}`,
-        clouds: `${FgWhite} clouds ${Reset}`,
-        rain: `${BgBlue}${FgWhite} rain ${Reset}`,
-        thunder: 'thunder\033[0m',
-        smoke: `${BgWhite}${FgBlack} smoke ${Reset}`,
-        haze: `${Dim} haze ${Reset}`,
-        snow: `${Bright} snow ${Reset}`,
-        fog: `${Dim} fog ${Reset}`,
-        wind: ` wind `,
-        tornado: ` tornado ` 
+        clear: `${ansiColors.bgLightBlue}${ansiColors.fgYellow} clear ${ansiColors.reset}`,
+        clouds: `${ansiColors.bgLightGray}${ansiColors.fgWhite} clouds ${ansiColors.reset}`,
+        rain: `${ansiColors.bgBlue}${ansiColors.fgBlack} rain ${ansiColors.reset}`,
+        thunder: `${ansiColors.bgLightGray}${ansiColors.fgMagenta} thunder  ${ansiColors.reset}`,
+        smoke: `${ansiColors.bgWhite}${ansiColors.fgLightRed} smoke ${ansiColors.reset}`,
+        haze: `${ansiColors.bgLightWhite}${ansiColors.dim}${ansiColors.fgLightGray} haze ${ansiColors.reset}`,
+        snow: `${ansiColors.bgLightGray} snow ${ansiColors.reset}`,
+        fog: `${ansiColors.bgLightWhite}${ansiColors.dim}${ansiColors.fgLightGray} fog ${ansiColors.reset}`,
+        wind: `${ansiColors.fgLightBlue}${ansiColors.bgLightGray} wind ${ansiColors.reset}`,
+        tornado: `${ansiColors.blink}${ansiColors.fgWhite} tornado! ${ansiColors.reset} ` 
     },
     icon: {
         clear: {
