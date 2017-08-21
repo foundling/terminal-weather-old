e5afcdcdf4f576cd60aae66ad79d7503550be4a9
Alex Ramsdell <alexramsdell@gmail.com>
2017-08-21 16:24:07 -0700

    consistency with other update tests.

--- cli/display.spec.js
+++ cli/display.spec.js
@@ -1,13 +1,18 @@
 const test = require('tape');
 const { setDisplay } = require('./display');
 
-const config = {
-    displayMode: 'text'
-} 
 
 test('display mode test', function(t) {
+
+    const config = {}; 
+    t.equal(config.displayMode, undefined);
+
+    setDisplay(config, 'text');
     t.equal(config.displayMode, 'text');
+
     setDisplay(config, 'icon');
     t.equal(config.displayMode, 'icon');
+
     t.end();
+
 });
