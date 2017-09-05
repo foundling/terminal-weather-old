3087c2346b6af258958b051f385aa356251f0f01
Alex Ramsdell <alexramsdell@gmail.com>
2017-09-05 10:23:48 -0700

    update todo, remove cli/update, add main to show.js

--- cli/show.js
+++ cli/show.js
@@ -1,14 +1,19 @@
-module.exports = function() {
+module.exports = {
+    main() {
 
-    const config = require(global.configPath);
-    console.log('CONFIG LOCATION:', global.configPath); 
-    console.log('API KEY:', config.API_KEY);
-    console.log('UNITS:', config.units);
-    console.log('NETWORK TIMEOUT THRESHOLD:', `${config.NETWORK_TIMEOUT_MS/1000.0} seconds` ); 
-    console.log('DISPLAY MODE:', config.displayMode);
-    if (!config.cache) return;
+        const config = require(global.configPath);
 
-    console.log('LAST CACHED WEATHER STRING:', config.cache.weather);
-    console.log('DATE LAST CACHED:', `${new Date(config.cache.lastCached).toLocaleTimeString()}, ${new Date(config.cache.lastCached).toDateString()}`);
+        console.log('CONFIG LOCATION:', global.configPath); 
+        console.log('API KEY:', config.API_KEY);
+        console.log('UNITS:', config.units);
+        console.log('NETWORK TIMEOUT THRESHOLD:', `${config.NETWORK_TIMEOUT_MS/1000.0} seconds` ); 
+        console.log('DISPLAY MODE:', config.displayMode);
 
-}
+        if (!config.cache) return;
+
+        console.log('LAST CACHED WEATHER STRING:', config.cache.weather);
+        console.log('DATE LAST CACHED:', `${new Date(config.cache.lastCached).toLocaleTimeString()}, ${new Date(config.cache.lastCached).toDateString()}`);
+
+    }
+
+};
