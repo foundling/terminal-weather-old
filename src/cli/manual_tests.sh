55f74765bea59c090d45755aabf97293bcd75056
Alex Ramsdell <alexramsdell@gmail.com>
2017-11-11 01:10:53 -0800

    add manual test

---
+++ src/cli/manual_tests.sh
@@ -0,0 +1,13 @@
+node bin/terminal-weather --help 
+node bin/terminal-weather show config
+node bin/terminal-weather --format 'D T'
+node bin/terminal-weather --no-cache
+node bin/terminal-weather 
+node bin/terminal-weather --prompt 
+node bin/terminal-weather -p 
+node bin/terminal-weather --display icon
+node bin/terminal-weather -n
+node bin/terminal-weather --format 'T D'
+node bin/terminal-weather --no-cache
+node bin/terminal-weather show config
+node bin/terminal-weather show display 
